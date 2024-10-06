cbf14ba31c0a37ddea6d04268e04ae4db2ccede2
me-cafebabe <me.cafebabe@gmail.com>
2024-10-06 08:45:07 +0200

    sm8150-common: Switch to QTI Memtrack AIDL HAL

--- common.mk
+++ common.mk
@@ -192,8 +192,6 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.4-service \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
-    android.hardware.memtrack@1.0-impl \
-    android.hardware.memtrack@1.0-service \
     gralloc.msmnile \
     hwcomposer.msmnile \
     libion.vendor \
@@ -202,7 +200,7 @@ PRODUCT_PACKAGES += \
     libtinyxml \
     libvulkan \
     libgui.vendor \
-    memtrack.msmnile \
+    vendor.qti.hardware.memtrack-service
     vendor.display.config@2.0 \
     vendor.display.config@2.0.vendor \
     vendor.qti.hardware.display.allocator-service \
